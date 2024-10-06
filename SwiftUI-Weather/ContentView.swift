0d88fa96a555ffb96758da141cc60370deb4b70e
Hector Merejo <Hector.Merejo@mail.citytech.cuny.edu>
2024-10-06 14:40:24 -0400

    Created UI for weather app

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,16 +9,130 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack{
+                BackgroundView(topColor: .blue, bottomColor: Color("lightBlue"))
+                VStack{
+                    CityTextView(cityName: "Cupertino, CA")
+                    
+                    MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
+                    
+                    HStack(spacing: 20) {
+                        WeatherDayView(dayOfWeek: "TUE",
+                                       imageName: "cloud.sun.fill",
+                                       temperature: 74)
+                        
+                        WeatherDayView(dayOfWeek: "WED",
+                                       imageName: "sun.max.fill",
+                                       temperature: 74)
+                        
+                        WeatherDayView(dayOfWeek: "THURS",
+                                       imageName: "wind.snow",
+                                       temperature: 74)
+                        
+                        WeatherDayView(dayOfWeek: "FRI",
+                                       imageName: "sunset.fill",
+                                       temperature: 74)
+                        
+                        WeatherDayView(dayOfWeek: "SAT",
+                                       imageName: "snow",
+                                       temperature: 74)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("tapped")
+                    } label: {
+                        Text("Change Day Time")
+                            .frame(width: 280, height: 50)
+                            .background(Color.white)
+                            .font(.system(size: 20, weight: .bold, design: .default))
+                            .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                    
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView: View {
+    
+    var cityName: String
+    
+    var body: some View {
+        
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+        
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    
+    var body: some View {
+        
+        VStack (spacing: 10) {
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+        }
+        
+        .padding(.bottom, 40)
+    }
+}
+
+
